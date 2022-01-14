2900f22d6445084635cff5e23128dc2860fd7ccf
louieiply <louieiply@gmail.com>
2022-01-14 14:12:27 +1030

    Update buttom ouput window with scrollbar

--- Youtube_Song_Download.py
+++ Youtube_Song_Download.py
@@ -2,7 +2,7 @@ from Observer import Observer
 from EventListener import Observable
 import os
 from tkinter import *
-from tkinter import filedialog, messagebox, ttk
+from tkinter import filedialog, messagebox, ttk, Listbox, Label, Canvas, Frame, Entry, Tk
 import youtubesearchpython
 from youtubesearchpython import ResultMode
 
@@ -51,7 +51,6 @@ class window():
         self.observerlist.attach(entity)
 
 
-
     def loadFile(self):
         try:
             if self.checkplaylist():
@@ -82,8 +81,8 @@ class window():
         self.root.update()
 
     def updatewindow(self,text):
-        new_output = Label(self.buttomFrame, text=text, bg="white", anchor="w")
-        new_output.pack(fill="both")
+        self.listbox.insert(self.linecount,text)
+        self.linecount = self.linecount + 1
         self.update()
 
     def check(self,app):
@@ -99,30 +98,40 @@ class window():
         self.canvas = Canvas(self.root, height=600, width=600, bg="#2c2f33")
         self.canvas.pack()
         self.entercountid = 0
+        self.linecount = 0
         self.observerlist = Observable()
         self.topFrame = Frame(self.root, bg="#7289da")
-        self.topFrame.place(relwidth=0.8, relheight=0.4, relx=0.1, rely=0.1)
+        self.topFrame.place(relwidth=0.8, relheight=0.4, relx=0.1, rely=0.1)    
         self.label = Label(self.topFrame, text="Enter the song list name:", bg="#7289da")
         self.label.place(relx=0.36, rely=0.16)
         self.entry = Entry(self.topFrame, text="Example playlist Name")
         self.entry.insert(0, "SongList")
         self.entry.place(relwidth=0.5, relheight=0.1, relx=0.25, rely=0.25, )
-        current_var = StringVar()
-        combobox = ttk.Combobox(self.topFrame, textvariable=current_var)
+        combobox = ttk.Combobox(self.topFrame, state="readonly")
         self.combobox = combobox
-        self.combobox["values"] = ("Download songs from local .txt",
-                                   "Download from youtube online playlist",)
+        self.combobox['values'] = ("Download songs from local .txt",
+                                   "Download from youtube online playlist")
         self.combobox.place(relwidth=0.5, relheight=0.1, relx=0.25, rely=0.37)
+        self.combobox.set("Download songs from local .txt")
         self.combobox.current(0)
+
         self.buttomFrame = Frame(self.root, bg="White")
         self.buttomFrame.place(relwidth=0.8, relheight=0.4, relx=0.1, rely=0.51)
+
+        self.listbox = Listbox(self.buttomFrame,bg="White")
+        self.listbox.pack(side="left", fill= BOTH, expand=1)
+
         self.scrollbar = Scrollbar(self.buttomFrame)
         self.scrollbar.pack(side="right", fill=Y)
+
+        self.listbox.config(yscrollcommand = self.scrollbar.set)
+        self.scrollbar.config(command = self.listbox.yview)
 
         self.myButton = Button(self.topFrame, text="Click here to Download", padx=10, pady=0, fg="#2c2f33",
                                bg="#99aab5",
                                command=self.loadFile)
         self.myButton.place(relx=0.34, rely=0.5)
+
 
 
 root = Tk()
